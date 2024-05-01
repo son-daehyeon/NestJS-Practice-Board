9b99b1620479a83fc89ca8d2c453b313b9d5f8fe
Son Daehyeon <sondaehyeon01@gmail.com>
2024-05-01 14:34:22 +0900

    chore: not display password field

--- src/domain/user/user.schema.ts
+++ src/domain/user/user.schema.ts
@@ -12,7 +12,7 @@ export class User {
   @Prop({ required: true, unique: true, index: true })
   email: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, select: false })
   password: string;
 }
 
