93cb6e2a6f511cef94059bec82252cdfe0748176
Son Daehyeon <sondaehyeon01@gmail.com>
2024-05-01 14:37:00 +0900

    chore: Add mongoose plugin - auto populate

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -7,7 +7,19 @@ import { UserRepository } from './user.repository';
 import { User, UserSchema } from './user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          schema.plugin(require('mongoose-autopopulate'));
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserRepository],
