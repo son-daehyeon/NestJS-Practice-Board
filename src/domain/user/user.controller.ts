6a7a3044803f1a25aa842045aefe4c4ce4efd93a
Son Daehyeon <sondaehyeon01@gmail.com>
2024-05-01 20:23:11 +0900

    feat: Get My info from token

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, UseGuards } from '@nestjs/common';
 
 import { UserService } from './user.service';
 
@@ -6,9 +6,19 @@ import { RegisterRequestDto } from './dto/request/RegisterRequestDto';
 import { LoginRequestDto } from './dto/request/LoginRequestDto';
 import { LoginResponseDto } from './dto/response/LoginResponseDto';
 
+import { User } from './user.schema';
+import { ReqUser } from './user.middleware';
+import { UserGuard } from './user.guard';
+
 @Controller('/api/user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get()
+  @UseGuards(UserGuard)
+  async me(@ReqUser() user: User): Promise<User> {
+    return user;
+  }
 
   @Put()
   async register(@Body() requestDto: RegisterRequestDto): Promise<void> {
