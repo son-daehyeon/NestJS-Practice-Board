855b71bacc35ea3950186ba38d6a990daaedc9f1
Son Daehyeon <sondaehyeon01@gmail.com>
2024-05-02 15:06:49 +0900

    feat: Add winston logger and morgan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+
 import { UserModule } from '../domain/user/user.module';
 import { BoardModule } from '../domain/board/board.module';
 import { CommentModule } from '../domain/comment/comment.module';
@@ -12,6 +16,38 @@ import { UserMiddleware } from '../domain/user/user.middleware';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+
+    WinstonModule.forRoot({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.ms(),
+        winston.format.printf(({ level, message, timestamp }) => {
+          return `${timestamp} [${level}] ${message}`;
+        }),
+      ),
+      transports: [
+        new winston.transports.Console(),
+
+        new winston.transports.DailyRotateFile({
+          level: 'info',
+          datePattern: 'YYYY-MM-DD',
+          dirname: './logs',
+          filename: `%DATE%.log`,
+          maxFiles: 30,
+          zippedArchive: true,
+        }),
+
+        new winston.transports.DailyRotateFile({
+          level: 'error',
+          datePattern: 'YYYY-MM-DD',
+          dirname: './logs/error',
+          filename: `%DATE%.error.log`,
+          maxFiles: 30,
+          zippedArchive: true,
+        }),
+      ],
     }),
 
     MongooseModule.forRootAsync({
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,40 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import { WINSTON_MODULE_NEST_PROVIDER, WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import * as morgan from 'morgan';
+
 import { AppModule } from './app/app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Nest Study - Board')
-    .setDescription('Implementing a board API using NestJS.')
-    .setVersion('1.0.0')
-    .addBearerAuth()
-    .build();
-  SwaggerModule.setup('/swagger-docs', app, SwaggerModule.createDocument(app, config), {
-    swaggerOptions: { defaultModelsExpandDepth: -1 },
-  });
+  SwaggerModule.setup(
+    '/swagger-docs',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Nest Study - Board')
+        .setDescription('Implementing a board API using NestJS.')
+        .setVersion('1.0.0')
+        .addBearerAuth()
+        .build(),
+    ),
+    {
+      swaggerOptions: { defaultModelsExpandDepth: -1 },
+    },
+  );
 
+  const logger = app.get(WINSTON_MODULE_PROVIDER);
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    morgan('[Response] :method :url :status :response-time ms', {
+      stream: { write: (message) => logger.info(message.split('\n')[0]) },
+    }),
+  );
 
   await app.listen(3000);
 })();
